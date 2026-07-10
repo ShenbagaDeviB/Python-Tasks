b3ea0163c777dadcfbedb18c8e8ca6aeb630a61f
ShenbagaDeviB <shenbagadevi3306@gmail.com>
2026-07-10 14:41:37 +0530

    Initial commit

---
+++ TasksinPython/Custom_Sort.py
@@ -0,0 +1,5 @@
+'''a=[1,2,5,4,8,3,6,7,9,10]
+print(sorted(a))'''
+a=[1,2,5,4,8,3,6,7,9,10]
+x=lambda a:sorted(a)
+print(x(a))
---
+++ TasksinPython/Palindrome_Checker.py
@@ -0,0 +1,5 @@
+s=input("Enter:")
+if s==s[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
