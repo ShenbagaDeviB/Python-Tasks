7de71aea047d50626c4e574a4c866f1a450cddf8
ShenbagaDeviB <shenbagadevi3306@gmail.com>
2026-07-16 10:34:34 +0530

    Utility Package Created

---
+++ TasksinPython/Utility_Package/main.py
@@ -0,0 +1,8 @@
+from utils import *
+print("Addition:",add(1,2))
+print("Subtraction:",sub(5,4))
+print("Multiplication:",multiply(5, 6))
+print("Division:",div(10, 2))
+print("Uppercase:",upper("hello"))
+print("Lowercase:",lower("WORLD"))
+print("Length:",length("Python"))
